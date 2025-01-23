b1a453f864cbfc5bd5c11ba123f92bc02fd5f74c
Prabha-Raj <116141429+Prabha-Raj@users.noreply.github.com>
2025-01-23 18:45:54 +0530

    checking slider

--- src/Components/slick.jsx
+++ src/Components/slick.jsx
@@ -1,34 +1,109 @@
-import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default function SimpleSlider() {
-  var settings = {
+const Slick = () => {
+  const settings = {
     dots: true,
-    infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
   };
+
   return (
-    <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
+    <div>
+      <h2>Carousel Component</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>FIRST SLIDE</h3>
+        </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+        <div>
+          <h3>FIRST SLIDE</h3>
+        </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+        <div>
+          <h3>FIRST SLIDE</h3>
+        </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+        <div>
+          <h3>FIRST SLIDE</h3>
+        </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+        <div>
+          <h3>FIRST SLIDE</h3>
+        </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+        <div>
+          <h3>FIRST SLIDE</h3>
+        </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+        <div>
+          <h3>FIRST SLIDE</h3>
+        </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+      </Slider>
+    </div>
   );
-}
+};
+
+export default Slick;
